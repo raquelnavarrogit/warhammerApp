92e59c4fbab030bf2fe1995f3dde8123781dc822
raquel <r.s.navarrorodriguez@gmail.com>
2025-05-12 21:24:34 +0200

    #1 CleanCode CodeStyle.

--- TFG/models/UserModel.java
+++ TFG/models/UserModel.java
@@ -10,7 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -21,7 +21,7 @@ public class UserModel {
     @Column(name = "email")
     private String email;
 
-    @Column(name = "username", nullable = false, unique=true)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
     @Column(name = "password", nullable = false)
