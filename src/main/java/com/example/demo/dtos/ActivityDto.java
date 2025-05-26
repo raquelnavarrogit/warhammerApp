40db73d134dc0ad2dddf801d54db827d1944cabb
raquel <r.s.navarrorodriguez@gmail.com>
2025-05-26 17:04:58 +0200

    #7 Clean code.

--- src/main/java/com/example/demo/dtos/ActivityDto.java
+++ src/main/java/com/example/demo/dtos/ActivityDto.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 public class ActivityDto {
 
     @NotBlank
-    private int id;
+    private Long id;
     @NotBlank
     private String name;
     private String description;
